2bfff76233fd87cd104cb2872ce56d944416ff88
shogohciai <shogo1104@gmail.com>
2013-07-03 00:22:16 +0900

    this is first commit.

---
+++ src/com/example/test1/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.test1;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+	public class LogTest extends Activity{
+		 
+	    /* TAG in Logcat */
+	    private static final String TAG = "LogTest";
+		
+	    /* ñàâÒSuperÇµÇƒÇÈÇÃÇ»ÇÒÇ≈Ç‚ÇÎ */
+	    @Override
+	    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+	    }
+	    
+	    /* onClickÇ≈testMethodåƒÇ—èoÇ∑ÇÊ */
+	    public void testMethod(View view){ 
+        PopupWindow(Context TAG);
+	    }
+	    
+	}
+
+
+    
+}
